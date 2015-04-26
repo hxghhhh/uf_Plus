5dba3006b85e1d5d8547da2ebd52250bcbc61901
hamt3ch <hmiles23@ufl.edu>
2015-04-25 23:40:11 -0400

    First Real Push

---
+++ Plus/Exercise.swift
@@ -0,0 +1,37 @@
+//
+//  Exercise.swift
+//  Plus
+//
+//  Created by Hugh A. Miles on 4/22/15.
+//  Copyright (c) 2015 HAMtech. All rights reserved.
+//
+
+import UIKit
+
+class Exercise: UIViewController {
+    
+    //let myNavBar: UINavigationBar = UINavigationBar(frame: CGRectZero)
+
+    
+    //UITableView(frame: CGRectZero, style: .Grouped)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        // change status bar colors to white letters
+        return UIStatusBarStyle.LightContent
+    }
+    
+    
+
+    
+   }
---
+++ Plus/Food.swift
@@ -0,0 +1,32 @@
+//
+//  Food.swift
+//  Plus
+//
+//  Created by Hugh A. Miles on 4/22/15.
+//  Copyright (c) 2015 HAMtech. All rights reserved.
+//
+
+import UIKit
+
+class Food: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        // change status bar colors to white letters
+        return UIStatusBarStyle.LightContent
+    }
+    
+    
+
+    
+    
+}
